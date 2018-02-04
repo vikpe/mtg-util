5f0129bf5847ba4c150d0e88dda7f4877ee23798
Viktor Persson <viktor.persson@arcsin.se>
2018-02-04 23:36:01 +0100

    Updated clean script. Replaced regex with glob.

--- clean.js
+++ clean.js
@@ -1,22 +1,14 @@
 // vendor
 const chalk = require('chalk');
 const fs = require('fs');
+const glob = require('glob');
 
 // config
 const config = require('./mtgUtilConfig');
 
 console.log(chalk`{green.bold MTG scan util (clean)}`);
 
-const combinedFileNameRegex = /card-\d+-c/g;
-
-const nonCombinedFilePaths = fs
-  .readdirSync(config.output.dir)
-  .filter(fileName => !combinedFileNameRegex.test(fileName))
-  .sort();
-
-console.log(chalk`{grey Found ${nonCombinedFilePaths.length} card(s)..}`);
-
-nonCombinedFilePaths.forEach(fileName => {
-  const filePath = `${config.output.dir}/${fileName}`;
-  fs.unlinkSync(filePath);
+glob(`${config.output.dir}/*-card-*-[ab].*`, (err, filesToDelete) => {
+  console.log(chalk`{grey Removing ${filesToDelete.length} file(s)..}`);
+  filesToDelete.forEach(filePath => fs.unlinkSync(filePath));
 });
