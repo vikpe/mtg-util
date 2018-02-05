dbc2bf43d882cf3728e9a963ff6689ba91a8ae73
Viktor Persson <viktor.persson@arcsin.se>
2018-02-05 03:15:08 +0100

    Refactored combine script.

--- combine.js
+++ combine.js
@@ -9,25 +9,22 @@ const imageUtil = require('./imageUtil');
 
 console.log(chalk`{green.bold MTG scan util (combine)}`);
 
-glob(`${config.output.dir}/*-card-*-a.*`, (err, frontSideScans) => {
+frontSideScans = glob.sync(`${config.output.dir}/*-card-*-a.*`);
 
-  console.log(chalk`{grey Found ${frontSideScans.length} card(s)..}`);
+console.log(chalk`{grey Found ${frontSideScans.length} card(s)..}`);
 
-  frontSideScans.forEach(fileName => {
-    const frontSidePath = fileName;
-    const backsidePath = frontSidePath.replace('-a.', '-b.');
-    const combinedPath = frontSidePath.replace('-a.', '-c.');
-    const basePath = frontSidePath.replace('-a.jpg', '');
+frontSideScans.forEach(fileName => {
+  const frontSidePath = fileName;
+  const backsidePath = frontSidePath.replace('-a.', '-b.');
+  const combinedPath = frontSidePath.replace('-a.', '-c.');
+  const basePath = frontSidePath.replace('-a.jpg', '');
 
-    let spinner = new ora().start(basePath);
+  let spinner = new ora().start(basePath);
 
-    imageUtil
-      .readImages([frontSidePath, backsidePath])
-      .then(images => imageUtil.combineHorizontally(images))
-      .then(combinedImage => combinedImage.write(combinedPath, () => {
-        spinner.succeed(basePath).stop();
-      }));
-  });
-
+  imageUtil
+    .readImages([frontSidePath, backsidePath])
+    .then(images => imageUtil.combineHorizontally(images))
+    .then(combinedImage => combinedImage.write(combinedPath, () => {
+      spinner.succeed(basePath).stop();
+    }));
 });
-
