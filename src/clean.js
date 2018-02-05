f4f2dc1c8ee627d9d76155a52e0f82f1c6b61f24
Viktor Persson <viktor.persson@arcsin.se>
2018-02-06 00:31:06 +0100

    Added progress spinner to clean script.

--- src/clean.js
+++ src/clean.js
@@ -2,11 +2,14 @@
 const chalk = require('chalk');
 const fs = require('fs');
 const glob = require('glob');
+const ora = require('ora');
 
 // custom
 const mtgUtil = require('./mtgUtil');
+const spinner = ora();
 
 const filesToClean = glob.sync(mtgUtil.globs.filesToClean);
 console.log(chalk`{green.bold MTG scan util (clean)}`);
-console.log(chalk`{grey Removing ${filesToClean.length} file(s)..}`);
+spinner.start(chalk`{grey Removing ${filesToClean.length} file(s)..}`);
 filesToClean.forEach(filePath => fs.unlinkSync(filePath));
+spinner.succeed().stop();
