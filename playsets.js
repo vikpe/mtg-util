23f09ba44114882c30e9da58618c39e5df5434b6
Viktor Persson <viktor.persson@arcsin.se>
2018-02-05 03:43:31 +0100

    Began work on automatically detecting playsets (WIP).

---
+++ playsets.js
@@ -0,0 +1,66 @@
+// vendor
+const chalk = require('chalk');
+const Jimp = require('jimp');
+const glob = require('glob');
+
+// config
+const imageUtil = require('./imageUtil');
+
+const similarityThreshold = 0.1;
+const thumbnailFilePaths = glob.sync('dist/*-card-*-t.*');
+
+console.log(chalk`{green.bold MTG scan util (find playsets)}`);
+console.log(chalk`{grey Found ${thumbnailFilePaths.length} card(s)..}`);
+
+imageUtil
+  .readImages(thumbnailFilePaths)
+  .then(imagesData => {
+    // get all distances
+    const imageDistances = {};
+    const imagesThatMatchAnotherImage = [];
+    const playsets = {};
+
+    imagesData.forEach((currentImage) => {
+      if (imagesThatMatchAnotherImage.includes(currentImage.fileName)) {
+        return;
+      }
+
+      imagesData.forEach((otherImage) => {
+        if (currentImage.fileName === otherImage.fileName) {
+          return;
+        }
+        else if (currentImage.sheetNumber !== otherImage.sheetNumber) {
+          return;
+        }
+
+        const key = [currentImage.fileName, otherImage.fileName].sort().join('|');
+
+        if (imageDistances.hasOwnProperty(key)) {
+          return;
+        }
+
+        const distance = Jimp.distance(
+          currentImage.thumbnail,
+          otherImage.thumbnail
+        );
+
+        imageDistances[key] = distance;
+
+        if (distance <= similarityThreshold) {
+          if (!playsets.hasOwnProperty(currentImage.fileName)) {
+            playsets[currentImage.fileName] = [currentImage.fileName];
+          }
+          playsets[currentImage.fileName].push(otherImage.fileName);
+          imagesThatMatchAnotherImage.push(otherImage.fileName);
+        }
+      });
+    });
+
+    // determine playsets
+    console.log(playsets);
+
+    Object.values(playsets).forEach(imageFileNames => {
+
+      console.log(imageFileNames);
+    });
+  });
