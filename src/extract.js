695969e19dc16376cf1a2fc30937876e0f8e309b
Viktor Persson <viktor.persson@arcsin.se>
2018-02-06 00:17:14 +0100

    Added queue in order to limit concurrency and fix spinner output.

--- src/extract.js
+++ src/extract.js
@@ -3,32 +3,27 @@ const chalk = require('chalk');
 const glob = require('glob');
 const Jimp = require('jimp');
 const ora = require('ora');
+const PQueue = require('p-queue');
 
 // custom
 const config = require('../mtgUtilConfig');
 const imageUtil = require('./imageUtil');
 const mtgUtil = require('./mtgUtil');
+const spinner = ora();
 
 const writeScan = scan => new global.Promise(resolve => {
   const distFilePath = `${config.output.dir}/${scan.sheetId}.jpg`;
+
+  spinner.start(chalk`${scan.imageSourcePath} {grey - overview}`);
 
   scan.image
       .clone()
       .resize(config.output.sheetWidth, Jimp.AUTO)
       .quality(config.output.imageQuality)
-      .write(distFilePath, () => resolve(scan));
-});
-
-const writePockets = scan => new global.Promise(resolve => {
-  const numberOfPocketsPerPage = config.sheet.rows * config.sheet.cols;
-  const promises = [];
-
-  for (let pocketIndex = 0; pocketIndex < numberOfPocketsPerPage; pocketIndex++) {
-    promises.push(writePocket(scan, pocketIndex));
-  }
-
-  global.Promise.all(promises)
-        .then(() => resolve(scan));
+      .write(distFilePath, () => {
+        spinner.succeed().stop();
+        resolve(scan);
+      });
 });
 
 const writePocket = (scan, pocketIndex) => new global.Promise(resolve => {
@@ -36,7 +31,7 @@ const writePocket = (scan, pocketIndex) => new global.Promise(resolve => {
   const pocketImage = imageUtil.cropPocketFromScan(scan.image, pocketIndex);
   const pocketId = `${scan.sheetId}-pocket-${pocketNumber}`;
 
-  console.log(chalk`${scan.imageSourcePath} - {grey pocket ${pocketNumber}}`);
+  spinner.start(chalk`{grey - pocket ${pocketNumber}}`);
 
   if ('auto' !== config.output.pocketWidth) {
     pocketImage.resize(config.output.pocketWidth, Jimp.AUTO);
@@ -48,9 +43,12 @@ const writePocket = (scan, pocketIndex) => new global.Promise(resolve => {
 
   const distFilePath = `${config.output.dir}/${pocketId}.jpg`;
 
-  return pocketImage
+  pocketImage
     .quality(config.output.imageQuality)
-    .write(distFilePath, () => resolve(scan));
+    .write(distFilePath, () => {
+      spinner.succeed().stop();
+      resolve(scan);
+    });
 });
 
 const writeCardArtwork = (cardImage, pocketId) => {
@@ -104,8 +102,17 @@ const scanFilePaths = glob.sync(mtgUtil.globs.scans).sort();
 console.log(chalk`{green.bold MTG scan util (extract)}`);
 console.log(chalk`{grey Found ${scanFilePaths.length} scan(s)..}`);
 
+const queue = new PQueue({ concurrency: 1 });
+const numberOfPocketsPerPage = config.sheet.rows * config.sheet.cols;
+
 scanFilePaths.forEach((filePath, index) => {
   getScan(filePath, index)
-    .then(scan => writeScan(scan))
-    .then(scan => writePockets(scan));
+    .then(scan => {
+
+      queue.add(() => writeScan(scan));
+
+      for (let pocketIndex = 0; pocketIndex < numberOfPocketsPerPage; pocketIndex++) {
+        queue.add(() => writePocket(scan, pocketIndex));
+      }
+    });
 });
