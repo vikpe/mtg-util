5676ae616c09c5b14dd3e5efa65a5844cc2c1ed4
Viktor Persson <viktor.persson@arcsin.se>
2018-04-23 20:24:44 +0200

    Increased default card output width.

--- mtgUtilConfig.js
+++ mtgUtilConfig.js
@@ -10,7 +10,7 @@ module.exports = {
     dir: 'dist',
     imageQuality: 90,
     sheetWidth: 1024,
-    cardWidth: 640, // a number or 'auto',
+    cardWidth: 512, // a number or 'auto',
     writeSheetFrontsides: true,
     writeArtwork: false
   }
