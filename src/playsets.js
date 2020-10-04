8fbcf148ca7e565fdec230db0d4fa82a579ae72b
Viktor Persson <viktor.persson@arcsin.se>
2020-10-04 13:29:28 +0200

    Update playsets.

--- src/playsets.js
+++ src/playsets.js
@@ -1,67 +1,122 @@
 // vendor
 const chalk = require("chalk");
+const ora = require("ora");
 const Jimp = require("jimp");
 const glob = require("glob");
+const { default: PQueue } = require("p-queue");
 
 // custom
 const imageUtil = require("./imageUtil");
 const mtgUtil = require("./mtgUtil");
+const spinner = ora();
 
-const similarityThreshold = 0.2;
+const similarityThreshold = 0.15;
 const artworkFilePaths = glob.sync(mtgUtil.globs.artwork);
 
 console.log(chalk`{green.bold MTG scan util (find playsets)}`);
 console.log(chalk`{grey Found ${artworkFilePaths.length} card(s)..}`);
 
-imageUtil
-  .readImages(artworkFilePaths)
-  .then(imagesData => {
-    // get all distances
-    const imageDistances = {};
-    const imagesThatMatchAnotherImage = [];
-    const playsets = {};
+const getPlaysets = (imagesData) => {
+  const imageDistances = {};
+  const imageDiffs = {};
+  const imagesThatMatchAnotherImage = [];
+  const playsets = {};
 
-    imagesData.forEach((currentImage) => {
-      if (imagesThatMatchAnotherImage.includes(currentImage.fileName)) {
+  // get all distances
+  imagesData.forEach((currentImage, currentIndex) => {
+    if (imagesThatMatchAnotherImage.includes(currentIndex)) {
+      return;
+    }
+
+    imagesData.forEach((otherImage, otherIndex) => {
+      if (currentIndex === otherIndex) {
         return;
       }
 
-      imagesData.forEach((otherImage) => {
-        if (currentImage.fileName === otherImage.fileName) {
-          return;
+      const key = [currentIndex, otherIndex].sort().join("x");
+
+      if (imageDistances.hasOwnProperty(key)) {
+        return;
+      }
+
+      // perceived distance
+      const distance = Jimp.distance(currentImage, otherImage);
+
+       // pixel difference
+      const diff = Jimp.diff(currentImage, otherImage);
+
+      imageDistances[key] = distance;
+      imageDiffs[key] = diff;
+
+      if (
+        distance <= similarityThreshold ||
+        diff.percent <= similarityThreshold
+      ) {
+        if (!playsets.hasOwnProperty(currentIndex)) {
+          playsets[currentIndex] = [currentIndex];
         }
-        else if (currentImage.sheetNumber !== otherImage.sheetNumber) {
-          return;
-        }
+        playsets[currentIndex].push(otherIndex);
+        imagesThatMatchAnotherImage.push(otherIndex);
+      }
 
-        const key = [currentImage.fileName, otherImage.fileName].sort().join('|');
-
-        if (imageDistances.hasOwnProperty(key)) {
-          return;
-        }
-
-        const distance = Jimp.distance(
-          currentImage.thumbnail,
-          otherImage.thumbnail
-        );
-
-        imageDistances[key] = distance;
-
-        if (distance <= similarityThreshold) {
-          if (!playsets.hasOwnProperty(currentImage.fileName)) {
-            playsets[currentImage.fileName] = [currentImage.fileName];
-          }
-          playsets[currentImage.fileName].push(otherImage.fileName);
-          imagesThatMatchAnotherImage.push(otherImage.fileName);
-        }
-      });
+      // free memory
+      imagesData[currentIndex] = null;
     });
+  });
 
-    // determine playsets
-    console.log(playsets);
+  console.log("imageDistances", imageDistances);
+  console.log("imageDiffs", imageDistances);
 
-    Object.values(playsets).forEach(imageFileNames => {
+  return playsets;
+};
 
-      console.log(imageFileNames);
+const writePlayset = (artworkFilePaths, destFilePath) => {
+  new global.Promise((resolve) => {
+    spinner.start(`Writing playset (${artworkFilePaths.length} images)`);
+
+    const frontSideFilePaths = artworkFilePaths.map((filePath) =>
+      filePath.replace("-artwork-", "-front-")
+    );
+    const backSideFilePaths = artworkFilePaths.map((filePath) =>
+      filePath.replace("-artwork-", "-back-")
+    );
+
+    const frontSidePromise = imageUtil
+      .readImages(frontSideFilePaths)
+      .then((images) => imageUtil.combineHorizontally(images));
+    const backSidePromise = imageUtil
+      .readImages(backSideFilePaths)
+      .then((images) => imageUtil.combineHorizontally(images));
+
+    Promise.all([frontSidePromise, backSidePromise])
+      .then((allSides) => imageUtil.combineVertically(allSides))
+      .then((playsetImage) => {
+        playsetImage.write(destFilePath, () => {
+          spinner.succeed().stop();
+          resolve();
+        });
+      });
+  });
+};
+
+const queue = new PQueue({ concurrency: 1 });
+
+imageUtil
+  .readImages(artworkFilePaths)
+  .then((imagesData) => {
+    const playsets = getPlaysets(imagesData);
+    const numberOfPlaysets = Object.keys(playsets).length;
+
+    console.log(chalk`{grey Found ${numberOfPlaysets} playset(s)..}`, playsets);
+    return playsets;
+  })
+  .then((playsets) => {
+    // write playsets
+    Object.values(playsets).forEach((imageIndexes, playsetIndex) => {
+      const filePaths = imageIndexes.map((index) => artworkFilePaths[index]);
+
+      queue.add(() =>
+        writePlayset(filePaths, `dist/playset-${1 + playsetIndex}.jpg`)
+      );
     });
   });
